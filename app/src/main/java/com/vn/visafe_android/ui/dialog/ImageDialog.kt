3fd1575bd2605c780f9f739d9c04e2d9ad240c54
nguyenducdung <nguyenducdung1803@gmail.com>
2021-07-11 22:09:10 +0700

    add ui home dashboard

---
+++ app/src/main/java/com/vn/visafe_android/ui/dialog/ImageDialog.kt
@@ -0,0 +1,85 @@
+package com.vn.visafe_android.ui.dialog
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.vn.visafe_android.R
+import com.vn.visafe_android.databinding.DialogImageBinding
+import com.vn.visafe_android.ui.create.group.access_manager.Action
+
+class ImageDialog : DialogFragment() {
+    companion object {
+        const val TYPE_DIALOG_KEY = "TYPE_DIALOG_KEY"
+
+        const val TYPE_ADD_VPN = "TYPE_ADD_VPN"
+        const val TYPE_TURN_ON_NOTI = "TYPE_TURN_ON_NOTI"
+
+        fun newsIntance(type: String) : ImageDialog {
+            val dialog = ImageDialog()
+            dialog.arguments = bundleOf(
+                Pair(TYPE_DIALOG_KEY, type)
+            )
+            return dialog
+        }
+    }
+    private lateinit var binding: DialogImageBinding
+    private var mOnClickListener: ((Action) -> Unit)? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.dialog_image,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        binding.lifecycleOwner = viewLifecycleOwner
+        initView()
+    }
+
+    private fun initView() {
+        binding.tvConfirm.setOnClickListener {
+            mOnClickListener?.invoke(Action.CONFIRM)
+            dismiss()
+        }
+        binding.ivClose.setOnClickListener {
+            dismiss()
+        }
+        val type = arguments?.getString(TYPE_DIALOG_KEY, "") ?: ""
+        if (type.isNotEmpty()) {
+            when (type) {
+                TYPE_ADD_VPN -> {
+                    binding.tvTitle.text = getString(R.string.add_vpn_dialog)
+                    binding.tvContent.text = getString(R.string.add_vpn_content)
+                    binding.tvConfirm.text = getString(R.string.cho_phep)
+                    binding.ivCheck.setImageResource(R.drawable.ic_vpn)
+                }
+                TYPE_TURN_ON_NOTI -> {
+                    binding.tvTitle.text = getString(R.string.turn_on_noti_title)
+                    binding.tvContent.text = getString(R.string.turn_on_noti_content)
+                    binding.tvConfirm.text = getString(R.string.bat_thong_bao)
+                    binding.ivCheck.setImageResource(R.drawable.ic_turn_on_noti)
+                }
+            }
+        }
+    }
+
+    fun setOnClickListener(onClickAction: (Action) -> Unit) {
+        mOnClickListener = onClickAction
+    }
+}
--- app/src/main/java/com/vn/visafe_android/ui/home/OverViewProtectFragment.kt
+++ app/src/main/java/com/vn/visafe_android/ui/home/OverViewProtectFragment.kt
@@ -1,6 +1,7 @@
 package com.vn.visafe_android.ui.home
 
 import android.content.Intent
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vn.visafe_android.R
 import com.vn.visafe_android.base.BaseFragment
@@ -13,7 +14,10 @@ import com.vn.visafe_android.ui.adapter.ApplicationMostAdapter
 import com.vn.visafe_android.ui.adapter.ContentMostAdapter
 import com.vn.visafe_android.ui.adapter.DeviceMostAdapter
 import com.vn.visafe_android.ui.adapter.GroupListAdapter
+import com.vn.visafe_android.ui.create.group.CreateGroupActivity
+import com.vn.visafe_android.ui.dialog.ImageDialog
 import com.vn.visafe_android.ui.group.dashboard.GroupDashboardActivity
+import com.vn.visafe_android.utils.setOnSingClickListener
 
 class OverViewProtectFragment : BaseFragment<FragmentOverViewProtectBinding>() {
 
@@ -25,33 +29,48 @@ class OverViewProtectFragment : BaseFragment<FragmentOverViewProtectBinding>() {
     override fun layoutRes(): Int = R.layout.fragment_over_view_protect
 
     override fun initView() {
-        val contentAdapter = ContentMostAdapter(createContentList())
-        binding.rvContentMost.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-        binding.rvContentMost.adapter = contentAdapter
-
-        val deviceAdapter = DeviceMostAdapter(createDeviceList())
-        binding.rvDeviceMost.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-        binding.rvDeviceMost.adapter = deviceAdapter
-
-        val appAdapter = ApplicationMostAdapter(createAppList())
-        binding.rvAppMost.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-        binding.rvAppMost.adapter = appAdapter
-
-        val groupAdapter = GroupListAdapter(createGroupList())
-        groupAdapter.onClickGroup = object : GroupListAdapter.OnClickGroup {
-            override fun openGroup(data: GroupData) {
-                val intent = Intent(requireContext(), GroupDashboardActivity::class.java)
-                intent.putExtra(GroupDashboardActivity.GROUP_DATA_KEY, data)
-                startActivity(intent)
-            }
-
-            override fun onClickMore() {
-
-            }
-
+//        val contentAdapter = ContentMostAdapter(createContentList())
+//        binding.rvContentMost.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+//        binding.rvContentMost.adapter = contentAdapter
+//
+//        val deviceAdapter = DeviceMostAdapter(createDeviceList())
+//        binding.rvDeviceMost.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+//        binding.rvDeviceMost.adapter = deviceAdapter
+//
+//        val appAdapter = ApplicationMostAdapter(createAppList())
+//        binding.rvAppMost.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+//        binding.rvAppMost.adapter = appAdapter
+//
+//        val groupAdapter = GroupListAdapter(createGroupList())
+//        groupAdapter.onClickGroup = object : GroupListAdapter.OnClickGroup {
+//            override fun openGroup(data: GroupData) {
+//                val intent = Intent(requireContext(), GroupDashboardActivity::class.java)
+//                intent.putExtra(GroupDashboardActivity.GROUP_DATA_KEY, data)
+//                startActivity(intent)
+//            }
+//
+//            override fun onClickMore() {
+//
+//            }
+//
+//        }
+//        binding.rvGroup.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+//        binding.rvGroup.adapter = groupAdapter
+        binding.layoutAddVpn.btnHomeVpnAdd.setOnSingClickListener {
+            showAddVpn()
         }
-        binding.rvGroup.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-        binding.rvGroup.adapter = groupAdapter
+        binding.layoutHomeProtectFamily.btnHomeFamilyAddGroup.setOnSingClickListener {
+            startActivity(Intent(requireContext(), CreateGroupActivity::class.java))
+        }
+        binding.layoutHomeProtect.switchHomeProtectDevice.setOnCheckedChangeListener { _, isChecked ->
+            binding.layoutHomeProtect.ivHomeProtectDevice.setImageResource(
+                if (isChecked) {
+                    R.drawable.ic_mobile
+                } else {
+                    R.drawable.ic_info_circle
+                }
+            )
+        }
     }
 
     private fun createContentList() : List<ContentMostData> {
@@ -87,5 +106,22 @@ class OverViewProtectFragment : BaseFragment<FragmentOverViewProtectBinding>() {
         list.add(GroupData("Phòng 3: Kinh tế - Đối ngoại", 20, 13, null))
         list.add(GroupData("Phòng 4: Chuyên gia", 20, 13, null))
         return list
+    }
+
+    private fun showAddVpn() {
+        val dialog = ImageDialog.newsIntance(ImageDialog.TYPE_ADD_VPN)
+        dialog.setOnClickListener {
+            Toast.makeText(requireContext(), "Add VPN", Toast.LENGTH_SHORT).show()
+        }
+        dialog.show(parentFragmentManager, null)
+    }
+
+    //Hiển thị dialog bật thông báo
+    private fun showDialogTurnOnNoti() {
+        val dialog = ImageDialog.newsIntance(ImageDialog.TYPE_TURN_ON_NOTI)
+        dialog.setOnClickListener {
+            Toast.makeText(requireContext(), "Add VPN", Toast.LENGTH_SHORT).show()
+        }
+        dialog.show(parentFragmentManager, null)
     }
 }
