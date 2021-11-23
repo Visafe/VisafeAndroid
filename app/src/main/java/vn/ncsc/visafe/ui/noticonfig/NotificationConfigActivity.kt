31060dfc05d22624cca3385e58e2165eafb93dcf
viethoang99 <viet.mta.hp@gmail.com>
2021-11-23 15:49:22 +0700

    implement for switch noti activity

--- app/src/main/java/vn/ncsc/visafe/ui/noticonfig/NotificationConfigActivity.kt
+++ app/src/main/java/vn/ncsc/visafe/ui/noticonfig/NotificationConfigActivity.kt
@@ -2,6 +2,7 @@ package vn.ncsc.visafe.ui.noticonfig
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import vn.ncsc.visafe.base.BaseActivity
 import vn.ncsc.visafe.databinding.ActivityNotificationConfigBinding
 import vn.ncsc.visafe.utils.OnSingleClickListener
@@ -23,5 +24,15 @@ class NotificationConfigActivity : BaseActivity() {
                 finish()
             }
         })
+        binding.switchProtect.setOnCheckedChangeListener {_, isChecked ->
+            if (binding.switchProtect.isChecked == true)
+            {
+                Toast.makeText(applicationContext, "Đã bật", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(applicationContext, "Đã tắt", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
